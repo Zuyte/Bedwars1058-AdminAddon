7f010c3ff1580b3db2818b0099497d9d768bc66b
Laptop <hunterzombie140@gmail.com>
2023-11-13 00:28:13 +0530

    update

--- src/main/java/me/zuyte/admin/support/BW2023.java
+++ src/main/java/me/zuyte/admin/support/BW2023.java
@@ -14,15 +14,19 @@ import org.bukkit.plugin.Plugin;
 
 public class BW2023 extends Addon implements IBedWars {
     private final Admin instance;
+
     public BW2023(Admin instance) {
         this.instance = instance;
         instance.bw2023 = Bukkit.getServicesManager().getRegistration(BedWars.class).getProvider();
         instance.bw2023.getAddonsUtil().registerAddon(this);
     }
+
     @Override
     public void setupMessages() {
-        for (Language lang : Language.getLanguages())
+        for (Language lang : Language.getLanguages()) {
             Messages.setupBedWarsMessages(lang.getYml());
+            lang.save();
+        }
     }
 
     @Override
@@ -35,8 +39,8 @@ public class BW2023 extends Addon implements IBedWars {
     @Override
     public void initializeListeners() {
         instance.getServer().getPluginManager().registerEvents(new ArenaListener(instance), instance);
-        instance.getServer().getPluginManager().registerEvents(new PlayerListener(instance) , instance);
-        instance.getServer().getPluginManager().registerEvents(new MenuListener(instance) , instance);
+        instance.getServer().getPluginManager().registerEvents(new PlayerListener(instance), instance);
+        instance.getServer().getPluginManager().registerEvents(new MenuListener(instance), instance);
         instance.getServer().getPluginManager().registerEvents(new TeamAssignerListener(instance), instance);
 
         if (Bukkit.getPluginManager().getPlugin("BedWars1058-TeamSelector") != null) { // Trying to hook into TeamSelector Addon
